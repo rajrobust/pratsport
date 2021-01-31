014288938cf511f57da59887304ab1eed43aeff6
Yash <guptayash134@gmail.com>
2021-01-31 22:04:38 +0530

    a

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.skills-content').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+}, {
+    offset: '80%'
+});
